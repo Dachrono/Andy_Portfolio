82d528ef974695833203ae2098ed66ece61d24dc
lRebornsl <anthony_vasquez98@hotmail.com>
2023-05-17 12:21:55 -0500

    Modified JS files and added data store.

--- JS/script.js
+++ JS/script.js
@@ -17,4 +17,76 @@ function cerrar() {
 }
 
 
-///for the first commit 
+// for the first commit 
+const cardsInfo = [
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    image: '',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+    live: '#',
+    source: '#',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    image: '',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+    live: '#',
+    source: '#',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    image: '',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+    live: '#',
+    source: '#',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    image: '',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+    live: '#',
+    source: '#',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    image: '',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+    live: '#',
+    source: '#',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    image: '',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+    live: '#',
+    source: '#',
+  },
+];
+
+const cards = document.querySelector('.Cards');
+const element = document.createElement('section');
+element.classList.add('WorksCard1');
+element.innerHTML=`
+  <div class="card1">
+    
+  </div>
+  <div class="card2">
+      <h1>Multi-Post Stories Gain+Glory</h1>
+  </div>
+  <ul class="card3">
+      <li>Ruby on Rails</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>HTML</li>
+  </ul>
+  <div class="card4">
+      <button class="CardBut">See project</button>
+  </div>
+`
+cards.appendChild(element);
