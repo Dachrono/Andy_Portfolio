1ae57c1bcd54adf90de0274c470d684679c023d2
Dachrono <bluefire_andy@hotmail.com>
2023-05-19 11:08:30 -0600

    Make the requested changes, change alert by text error message

--- JS/validation.js
+++ JS/validation.js
@@ -1,12 +1,11 @@
 const conForm = document.querySelector('.ContForm-cont');
 const mail = document.getElementById('email');
+const errMess = document.querySelector('.errMess');
+errMess.style.display = 'none';
 
 conForm.addEventListener('submit', (e) => {
   if (mail.value !== mail.value.toLowerCase()) {
     e.preventDefault();
-    const emailLC = mail.value.toLowerCase();
-    mail.value = emailLC;
-    // eslint-disable-next-line no-alert
-    alert('Please enter your email in all lowercase, we do it for you only check it if it is ok');
+    errMess.style.display = 'block';
   }
 });
