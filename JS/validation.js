94bc46f35b091471fd8119cd898c7be83fda78f3
lRebornsl <anthony_vasquez98@hotmail.com>
2023-05-19 11:26:41 -0500

    Modified JS file

--- JS/validation.js
+++ JS/validation.js
@@ -4,5 +4,9 @@ const mail = document.getElementById('email');
 conForm.addEventListener('submit', (e) => {
   if (mail.value !== mail.value.toLowerCase()) {
     e.preventDefault();
+    const emailLC = mail.value.toLowerCase();
+    mail.value = emailLC;
+    // eslint-disable-next-line no-alert
+    alert('Please enter your email in all lowercase, we do it for you only check it if it is ok');
   }
 });
